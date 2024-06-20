31224610cea5741f17b573c11b4438f7d9ed8d25
interceptic <sambaksht@gmail.com>
2024-06-20 18:42:55 +0900

    sql table

--- database/sqlite.py
+++ database/sqlite.py
@@ -4,4 +4,17 @@ from discord.ext import commands
 
 # IDK HOW TO DO SQLITE 
 
-async def setup()
+async def setup():
+    async with aiosqlite.connect('./database/database.db') as sqlite:
+        await database.execute('''
+            CREATE TABLE IF NOT EXISTS [info] (
+                [guild_id] INTEGER,
+                [ign] TEXT,
+                [channel_id] INTEGER,
+                [message_id] INTEGER,
+                [price] FLOAT,
+                [payment_methods] TEXT,
+                [seller_discord_id] INTEGER,
+                [buyer_discord_id] INTEGER
+            )
+        ''')
